941cdab86e2ec29314606ea7acb3293878b60df3
Aamer Sadiq <aamer_au@yahoo.com>
2016-06-09 15:38:57 +1000

    added title service

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,11 @@
 import { bootstrap }  from '@angular/platform-browser-dynamic';
 import { AppComponent } from './modules/core/app.component';
 import { HTTP_PROVIDERS } from '@angular/http';
-bootstrap(AppComponent, [HTTP_PROVIDERS]);
+import { Title } from '@angular/platform-browser';
+bootstrap(AppComponent, [HTTP_PROVIDERS, Title ]).then(
+    () => window.console.info( 'Angular finished bootstrapping your application!' ),
+    (error) => {
+      console.warn( 'Angular was not able to bootstrap your application.' );
+      console.error( error );
+    }
+  );;
--- app/modules/core/app.component.ts
+++ app/modules/core/app.component.ts
@@ -1,5 +1,6 @@
   import { Component }       from '@angular/core';
   import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+  import { Title }     from '@angular/platform-browser';
 
   import { WineService }  from '../shared/services/wine.service';
   import { DashboardComponent } from '../dashboard/dashboard.component';
@@ -8,10 +9,10 @@
   @Component({
     selector: 'my-app',
     template: `
-      <h1>{{title}}</h1>
+      <h1>{{header}}</h1>
        <nav>
-        <a [routerLink]="['Dashboard']">Dashboard</a>
-        <a [routerLink]="['Wines']">Wines</a>
+        <a [routerLink]="['Dashboard']" (click)="setTitle( 'Dashboard' )">Dashboard</a>
+        <a [routerLink]="['Wines']" (click)="setTitle( 'Wines' )">Wines</a>
       </nav>
       <router-outlet></router-outlet>
     `,
@@ -43,5 +44,9 @@
   ])
 
   export class AppComponent {
-    title = 'Cellar Master';
+    public constructor(private titleService: Title ) { }
+     public setTitle( newTitle: string) {
+        this.titleService.setTitle( newTitle );
+     }
+    header = 'Cellar Master';
   }
